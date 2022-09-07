d710e241270d9e98f44c58b600e0fc8a01df8b1b
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-09-07 17:54:49 +0200

    Debug test

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -12,10 +12,8 @@ EXAMPLES_DIR = os.path.join(rootdir, 'examples')
 
 @pytest.mark.parametrize('dirname', os.listdir(EXAMPLES_DIR))
 def test_examples(dirname):
-    with open(os.devnull, 'w') as devnull:
+    with open(os.devnull, 'w'):
         assert subprocess.call(
             [sys.executable, '-mmkdocs', 'build'],
             cwd=os.path.join(EXAMPLES_DIR, dirname),
-            stdout=devnull,
-            stderr=devnull,
         ) == 0
