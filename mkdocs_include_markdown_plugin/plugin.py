c460105805242c9825c37172fb29a3cb0d3d0ccf
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-09-28 19:07:48 +0200

    Bump version

--- mkdocs_include_markdown_plugin/plugin.py
+++ mkdocs_include_markdown_plugin/plugin.py
@@ -6,49 +6,66 @@ from mkdocs_include_markdown_plugin.event import (
 )
 
 
-class WatchingFiles:
-    def __init__(self):
-        self.prev_included_files = []
-        self.included_files = []
+mkdocs__version_info__ = tuple(
+    int(num) for num in mkdocs.__version__.split('.') if num.isdigit()
+)
 
+if mkdocs__version_info__ < (1, 4, 0):
+    class IncludeMarkdownPlugin(mkdocs.plugins.BasePlugin):
+        config_scheme = CONFIG_SCHEME
 
-SERVER = None
-WATCHING_FILES = None
+        def on_page_markdown(self, markdown, page, **kwargs):
+            return _on_page_markdown(
+                markdown,
+                page,
+                kwargs['config']['docs_dir'],
+                config=self.config,
+                build=None,
+            )
+else:
 
+    class WatchingFiles:
+        def __init__(self):
+            self.prev_included_files = []
+            self.included_files = []
 
-class IncludeMarkdownPlugin(mkdocs.plugins.BasePlugin):
-    config_scheme = CONFIG_SCHEME
+    SERVER = None
+    WATCHING_FILES = None
 
-    def _watch_included_files(self):
-        for filepath in WATCHING_FILES.prev_included_files:
-            if filepath not in WATCHING_FILES.included_files:
-                SERVER.unwatch(filepath)
-        WATCHING_FILES.prev_included_files = WATCHING_FILES.included_files[:]
+    class IncludeMarkdownPlugin(mkdocs.plugins.BasePlugin):
+        config_scheme = CONFIG_SCHEME
 
-        for filepath in WATCHING_FILES.included_files:
-            SERVER.watch(filepath, recursive=False)
-        WATCHING_FILES.included_files = []
+        def _watch_included_files(self):
+            for filepath in WATCHING_FILES.prev_included_files:
+                if filepath not in WATCHING_FILES.included_files:
+                    SERVER.unwatch(filepath)
+            WATCHING_FILES.prev_included_files = (
+                WATCHING_FILES.included_files[:]
+            )
 
-    def on_page_content(self, html, *args, **kwargs):
-        if SERVER:
-            self._watch_included_files()
-        return html
+            for filepath in WATCHING_FILES.included_files:
+                SERVER.watch(filepath, recursive=False)
+            WATCHING_FILES.included_files = []
 
-    def on_serve(self, server, builder, **kwargs):
-        global SERVER
-        if SERVER is None:
-            SERVER = server
-            self._watch_included_files()
+        def on_page_content(self, html, *args, **kwargs):
+            if SERVER:
+                self._watch_included_files()
+            return html
 
-    def on_page_markdown(self, markdown, page, **kwargs):
-        global WATCHING_FILES
-        if WATCHING_FILES is None:
-            WATCHING_FILES = WatchingFiles()
-        kwargs['build'] = WATCHING_FILES
-        return _on_page_markdown(
-            markdown,
-            page,
-            kwargs['config']['docs_dir'],
-            config=self.config,
-            build=kwargs['build'],
-        )
+        def on_serve(self, server, builder, **kwargs):
+            global SERVER
+            if SERVER is None:
+                SERVER = server
+                self._watch_included_files()
+
+        def on_page_markdown(self, markdown, page, **kwargs):
+            global WATCHING_FILES
+            if WATCHING_FILES is None:
+                WATCHING_FILES = WatchingFiles()
+            return _on_page_markdown(
+                markdown,
+                page,
+                kwargs['config']['docs_dir'],
+                config=self.config,
+                build=WATCHING_FILES,
+            )
