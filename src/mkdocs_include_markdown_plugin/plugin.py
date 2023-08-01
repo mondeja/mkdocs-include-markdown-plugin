b812e8bac42a1733f3f4c82197a0d605fb1c41d1
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2023-08-01 08:03:55 +0200

    Remove Mkdocs<1.4.0 branch

--- src/mkdocs_include_markdown_plugin/plugin.py
+++ src/mkdocs_include_markdown_plugin/plugin.py
@@ -61,23 +61,20 @@ class IncludeMarkdownPlugin(BasePlugin):
     def _watch_included_files(self) -> None:
         global FILES_WATCHER, SERVER
         SERVER = cast(LiveReloadServer, SERVER)
+        FILES_WATCHER = cast(FilesWatcher, FILES_WATCHER)
 
-        # compatibility with Mkdocs < 1.4.0
-        if hasattr(SERVER, 'unwatch'):
-            FILES_WATCHER = cast(FilesWatcher, FILES_WATCHER)
+        # unwatch previous watched files not needed anymore
+        for filepath in FILES_WATCHER.prev_included_files:
+            if filepath not in FILES_WATCHER.included_files:
+                SERVER.unwatch(filepath)
+        FILES_WATCHER.prev_included_files = (
+            FILES_WATCHER.included_files[:]
+        )
 
-            # unwatch previous watched files not needed anymore
-            for filepath in FILES_WATCHER.prev_included_files:
-                if filepath not in FILES_WATCHER.included_files:
-                    SERVER.unwatch(filepath)
-            FILES_WATCHER.prev_included_files = (
-                FILES_WATCHER.included_files[:]
-            )
-
-            # watch new included files
-            for filepath in FILES_WATCHER.included_files:
-                SERVER.watch(filepath, recursive=False)
-            FILES_WATCHER.included_files = []
+        # watch new included files
+        for filepath in FILES_WATCHER.included_files:
+            SERVER.watch(filepath, recursive=False)
+        FILES_WATCHER.included_files = []
 
     def on_page_content(
             self,
