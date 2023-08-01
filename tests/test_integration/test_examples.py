be26ee065d61a9e8e9197a4943b0b0103bbec0f3
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2023-08-01 21:03:38 +0200

    Refactor tests

--- tests/test_integration/test_examples.py
+++ tests/test_integration/test_examples.py
@@ -14,16 +14,18 @@ from mkdocs_include_markdown_plugin.cache import CACHE_AVAILABLE
 EXAMPLES_DIR = os.path.join(rootdir, 'examples')
 
 
+def config_is_using_cache_setting(config_file_path):
+    with open(config_file_path, encoding='utf-8') as f:
+        return 'cache:' in f.read()
+
+
 @pytest.mark.parametrize('dirname', os.listdir(EXAMPLES_DIR))
 def test_examples_subprocess(dirname):
-    expected_returncode = 0
-
     example_dir = os.path.join(EXAMPLES_DIR, dirname)
     config_file = os.path.join(example_dir, 'mkdocs.yml')
-    with open(config_file, encoding='utf-8') as f:
-        if 'cache:' in f.read():
-            if not CACHE_AVAILABLE:
-                expected_returncode = 1
+    expected_returncode = 1 if config_is_using_cache_setting(
+        config_file,
+    ) and not CACHE_AVAILABLE else 0
 
     proc = subprocess.Popen(
         [sys.executable, '-mmkdocs', 'build'],
@@ -40,13 +42,11 @@ def test_examples_subprocess(dirname):
 
 @pytest.mark.parametrize('dirname', os.listdir(EXAMPLES_DIR))
 def test_examples_api(dirname):
-    expected_to_raise_exc = False
     example_dir = os.path.join(EXAMPLES_DIR, dirname)
     config_file = os.path.join(example_dir, 'mkdocs.yml')
-    with open(config_file, encoding='utf-8') as f:
-        if 'cache:' in f.read():
-            if not CACHE_AVAILABLE:
-                expected_to_raise_exc = True
+    expected_to_raise_exc = (
+        config_is_using_cache_setting(config_file) and not CACHE_AVAILABLE
+    )
 
     def run():
         cfg = config.load_config(config_file=config_file)
